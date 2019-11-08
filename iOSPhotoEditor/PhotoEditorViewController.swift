8b5701335b0e8e2bc0c2bebdbf3ce11d9b7be7bb
Andrea Ongaro <andrea.ongaro.kuro@gmail.com>
2019-11-08 16:33:30 +0100

    Adding objc support

--- iOSPhotoEditor/PhotoEditorViewController.swift
+++ iOSPhotoEditor/PhotoEditorViewController.swift
@@ -39,18 +39,18 @@ public final class PhotoEditorViewController: UIViewController {
     @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var clearButton: UIButton!
     
-    public var image: UIImage?
+    @objc public var image: UIImage?
     /**
      Array of Stickers -UIImage- that the user will choose from
      */
-    public var stickers : [UIImage] = []
+    @objc public var stickers : [UIImage] = []
     /**
      Array of Colors that will show while drawing or typing
      */
-    public var colors  : [UIColor] = []
+    @objc public var colors  : [UIColor] = []
     
-    public var photoEditorDelegate: PhotoEditorDelegate?
-    var colorsCollectionViewDelegate: ColorsCollectionViewDelegate!
+    @objc public var photoEditorDelegate: PhotoEditorDelegate?
+    @objc var colorsCollectionViewDelegate: ColorsCollectionViewDelegate!
     
     // list of controls to be hidden
     public var hiddenControls : [control] = []
